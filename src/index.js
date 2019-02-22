6832d57f3c0dc6eec9f1cd7cd2b3042312484384
Maksim Shabanski <shabanskij.maksim@gmail.com>
2019-02-22 20:37:41 +0300

    Solution love-triangle task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,30 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var personLength = preferences.length;
+  var excludedPersons = {};
+  var loveTrianglesCount = 0;
+
+  for (var person = 0; person < personLength; person++) {
+    var nextPerson = preferences[person] - 1;
+    var loveTriangle = [nextPerson];
+
+    if (excludedPersons[nextPerson]) continue;
+    if (person === nextPerson) continue;
+
+    for (var i = 0; i < 2; i++) {
+      nextPerson = preferences[nextPerson] - 1;
+      loveTriangle.push(nextPerson);
+    }
+
+    if (person === nextPerson) {
+      loveTriangle.forEach(function(person) {
+        excludedPersons[person] = true;
+      });
+
+      loveTrianglesCount++;
+    }
+  }
+
+  return loveTrianglesCount;
 };
